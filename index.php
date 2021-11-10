21768d48de3fc8fd9c09f701977bd952b595affe
kimia <farshadniakimia@gmail.com>
2021-11-10 21:47:52 +0330

    first commit all files for push on github

---
+++ index.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="style.css">
+    <title></title>
+  </head>
+  <body>
+    <div id="section1"  class="container-fluid">
+      <div id="img1-1">
+        <img src="images/Muhanna-Durra-Transparencies-1-web-410x500.jpg" id="img1" alt="" height="500" width="500">
+        <img src="images/IMG_5236.jpg" id="img2" alt="" height="300" width="200">
+        <img src="images/1-transparency-ii-megan-meagher.jpg" id="img3" alt="" height="100" width="100">
+      </div>
+      <div id="text1">
+        <h1>INTERSECTION</h1>
+        <p>Transparency</p>
+      </div>
+    </div>
+
+    <div id="section2"  class="container-fluid">
+      <div id="img2-2">
+        <img src="images/6 slide.png" id="img4" alt="" width="600" height="400">
+      </div>
+      <div id="text2">
+          <p id="text2-1">While their intersection<br>their overlapping, their interlocking.</p>
+        </div>
+      <div id="text2-2">
+        <p>Both systems of coordinates provide for the orientation of the figures simultaneously in an extended space and on a painted surface; while their intersection, their overlapping, their interlocking, and their building up into larger and fluctuating configurations permits the genesis of the typically ambiguous cubist motif.</p>
+
+      </div>
+    </div>
+
+    <div id="section3"  class="container-fluid">
+      <div id="img3-3">
+          <img src="images/trans-6 side.png" id="img5" alt="" width="300" height="400">
+
+            <img src="images/cricketfineart-bob-aldous-transparency-of-light-london-gallery.jpg" id="img6" alt="" width="500" height="625">
+
+              <img src="images/1-transparency-ii-megan-meagher1.jpg" id="img7" alt="" width="200" height="200">
+      </div>
+      <div id="text3">
+        <p id="text3-1">Transparency can be considered in order to achieve a spatial
+                  continuity inside and outside.</p>
+          <h1 id="text3-2">Portfolio</h1>
+      </div>
+
+    </div>
+
+    <div id="section4"  class="container-fluid">
+
+      <div id="img4-4">
+          <img src="images/26.jpg" id="img8" alt="" width="1305" height="580">
+      </div>
+      <div id="text4">
+        <p id="text4-1">It is experienced in the space of thought, feeling, imagination,  ideal or experience within human beings, perception, feeling  or desire.</p>
+      </div>
+    </div>
+
+    <div id="section5"  class="container-fluid">
+        <div id="text5">
+          <p id="text5-1">While their intersection, their overlapping, their interlocking</p>
+          <p id="text5-2">Both systems of coordinates provide
+              for the orientation of the figures simultaneously
+              in an extended space and on
+              a painted surface; while their intersection,
+              their overlapping, their interlocking, and
+              their building up into larger and fluctuating
+              configurations permits the genesis of
+              the typically ambiguous cubist motif.
+          </p>
+        </div>
+
+        <div id="img5-5">
+          <img src="images/IMG_5242.jpg" id="img9" alt="" width="320" height="220">
+          <img src="images/IMG_5244.jpg" id="img10" alt="" width="320" height="220">
+        </div>
+        <div id="transparent-text1">
+          <p>'Transparency,' 'space-time,' 'simultaneity,'
+              'interpenetration,' 'superimposition,'
+              'ambivalence': in the literature of contemporary
+              architecture these words, and others like them,are often used as synonyms.</p>
+        </div>
+
+        <div id="transparent-text2">
+          <p>Generally speaking, the oblique
+and curved lines possess a certain naturalistic
+significance, while the rectilinear ones show a geometrizing tendency which serves as a reassertion of the picture plane</p>
+        </div>
+
+    </div>
+  </body>
+</html>
